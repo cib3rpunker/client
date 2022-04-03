b294313151755027e5388dadd5f1f9a82dc85ac2
ciberpunk3r <ciberpunk3r@gmail.com>
2022-04-03 07:57:02 -0500

    📂3. React Basics
    🎥 Videos 📺
    1. Section 3 Introduction
    2 🥣 Creating the react application 💊 npx create-react-app --template typescript
    3. What's in the React template
    4. React component basics
    5. Using react hooks - useState
    6. Using react hooks - useEffect
    7. Adding CORS configuration to the API
    8. Typescript vs javascript in React

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,44 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from 'react'
 
 function App() {
+  const [products, setProducts] = useState([
+    { id: 1, name: 'product1', price: 100.0 },
+    { id: 2, name: 'product2', price: 100.0 },
+  ])
+
+  useEffect(() => {
+    fetch('http://localhost:5000/api/products')
+      .then((response) => response.json())
+      .then((data) => setProducts(data))
+  }, [])
+
+  function addProduct() {
+    // setProducts([...products, { name: 'product3', price: 300.0 }])
+    setProducts((prevState) => [
+      ...prevState,
+      {
+        id: prevState.length + 1,
+        name: 'product' + (prevState.length + 1),
+        price: Math.random() * 1000,
+      },
+    ])
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Re-Store</h1>
+      <ul>
+        {products.map((product) => {
+          return (
+            <li key={product.id}>
+              {product.name}: {product.price}
+            </li>
+          )
+        })}
+      </ul>
+      <button onClick={addProduct}>Add Product</button>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
