dbd76d23f2a9ccc1659e296ba8e679e129b27c4d
ciberpunk3r <ciberpunk3r@gmail.com>
2022-04-05 19:02:07 -0500

    📂3. React Basics 🎥11. Adding React components

--- src/app/layout/App.tsx
+++ src/app/layout/App.tsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from 'react'
-import { Product } from './product'
+import Catalog from '../../features/Catalog'
+import { Product } from '../models/product'
 
 function App() {
   const [products, setProducts] = useState<Product[]>([])
@@ -29,16 +30,7 @@ function App() {
   return (
     <div>
       <h1>Re-Store</h1>
-      <ul>
-        {products.map((product) => {
-          return (
-            <li key={product.id}>
-              {product.name}: {product.price}
-            </li>
-          )
-        })}
-      </ul>
-      <button onClick={addProduct}>Add Product</button>
+      <Catalog products={products} addProduct={addProduct}/>
     </div>
   )
 }
