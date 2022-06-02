8d8a83516780a807aeb02883bfe96ca34e80d7e2
ciberpunk3r <ciberpunk3r@gmail.com>
2022-06-01 22:23:47 -0500

    📁3. React Basics 📺16. Adding cards for our individual products

--- src/features/catalog/ProductCard.tsx
+++ src/features/catalog/ProductCard.tsx
@@ -1,21 +1,34 @@
-import { ListItem, ListItemAvatar, Avatar, ListItemText } from "@mui/material";
-import { Product } from "../../app/models/product";
+import { Button, Card, CardActions, CardContent, CardMedia, Typography } from '@mui/material'
+import { Product } from '../../app/models/product'
 
 interface Props {
-  product: Product;
+  product: Product
 }
 
-export default function ProductCard({product}: Props) {
+export default function ProductCard({ product }: Props) {
   return (
     <>
-      <ListItem key={product.id}>
-        <ListItemAvatar>
-          <Avatar src={product.pictureUrl} />
-        </ListItemAvatar>
-        <ListItemText>
-          {product.name}: {product.price}
-        </ListItemText>
-      </ListItem>
+      <Card >
+        <CardMedia
+          component="img"
+          height="140"
+          image="https://picsum.photos/200"
+          alt="green iguana"
+        />
+        <CardContent>
+          <Typography gutterBottom variant="h5" component="div">
+            Lizard
+          </Typography>
+          <Typography variant="body2" color="text.secondary">
+            Lizards are a widespread group of squamate reptiles, with over 6,000
+            species, ranging across all continents except Antarctica
+          </Typography>
+        </CardContent>
+        <CardActions>
+          <Button size="small">Share</Button>
+          <Button size="small">Learn More</Button>
+        </CardActions>
+      </Card>
     </>
   )
 }
--- src/features/catalog/ProductList.tsx
+++ src/features/catalog/ProductList.tsx
@@ -1,20 +1,21 @@
-import { List } from "@mui/material"
-import { Product } from "../../app/models/product";
-import ProductCard from "./ProductCard";
+import { Grid } from '@mui/material'
+import { Product } from '../../app/models/product'
+import ProductCard from './ProductCard'
 
 interface Props {
-  products: Product[];
-  // addProduct: () => void;
+  products: Product[]
 }
 
-export default function ProductList({products, /* addProduct */}: Props) {
+export default function ProductList({ products }: Props) {
   return (
-    <List>
+    <Grid container spacing={4}>
       {products.map((product) => {
         return (
-          <ProductCard key={product.id} product={product}/>
+          <Grid item xs={4} key={product.id}>
+            <ProductCard product={product} />
+          </Grid>
         )
       })}
-    </List>
+    </Grid>
   )
 }
