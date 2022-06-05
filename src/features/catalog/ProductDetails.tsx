ccc835c2d97ca0a38953fb9fd7fbb386e0c08f99
ciberpunk3r <ciberpunk3r@gmail.com>
2022-06-05 10:29:55 -0500

    📁4. React Router 📺5. Fetching a product on component load

--- src/features/catalog/ProductDetails.tsx
+++ src/features/catalog/ProductDetails.tsx
@@ -1,9 +1,32 @@
 import { Typography } from '@mui/material'
+import axios from 'axios'
+import { useEffect, useState } from 'react'
+import { useParams } from 'react-router-dom'
+import { Product } from '../../app/models/product'
 
 export default function ProductDetails() {
+  const {id} = useParams<{id: string}>()
+  const [product, setProduct] = useState<Product | null>(null)
+  const [loading, setLoading] = useState(true)
+
+  useEffect(() => {
+    axios.get(`http://localhost:5000/api/products/${id}`)
+    .then(res => setProduct(res.data))
+    .catch(err => console.log(err))
+    .finally(() => setLoading(false))
+  })
+
+  if(loading) {
+    return <h3>Loading...</h3>
+  }
+
+  if(!product) {
+    return <h3>Product not found</h3>
+  }
+
   return (
     <>
-      <Typography variant="h2">Product Details</Typography>
+      <Typography variant="h2">{product.name}</Typography>
     </>
   )
 }
